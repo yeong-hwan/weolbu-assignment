b5f2cdf8fdfb06ced810d805745b46cdd6647fae
yeong-hwan <jghff700@naver.com>
2024-11-20 21:17:18 +0900

    refactor(member): append soft-delete field at member

--- src/main/java/com/weolbu/core/member/domain/Member.java
+++ src/main/java/com/weolbu/core/member/domain/Member.java
@@ -2,15 +2,30 @@ package com.weolbu.core.member.domain;
 
 import com.weolbu.core.enums.MemberType;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Where;
 
-@Entity
 @Getter
 @Setter
+@Builder
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Where(clause = "is_deleted = false")
 public class Member {
-    @Id @GeneratedValue
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
+
+    @Builder.Default
+    private boolean isDeleted = Boolean.FALSE;
 
     private String name;
     private String email;
