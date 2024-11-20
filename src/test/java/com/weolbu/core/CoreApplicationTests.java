cb89af8e78cf697a63578917ce9d9d2916e6dd2f
yeong-hwan <jghff700@naver.com>
2024-11-20 20:39:24 +0900

    feat: set up for spring boot

---
+++ src/test/java/com/weolbu/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weolbu.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
